b6aab8676df816d80c90eecc08aaf75ad9a6f24a
maltsev <arelav90@mail.ru>
2018-06-13 17:02:40 +0300

    final first iteration

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+This script can list your online friends.
 
 # How to Install
 
 Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
 
-```bash
+```cmd
 pip install -r requirements.txt # alternatively try pip3
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+# How to Use
+
+To use script you need to enter login and password. Output will be your online VK friends.
+
+Example (Windows):
+
+```cmd
+C:\>python C:\8_vk_friends_online\vk_friends_online.py
+
+Enter your login: your_login 
+Enter your password: your_password
+
+List of online users:
+
+Nika   Sokolova
+Катерина   Кузнецова
+Алексей   Петровский
+```
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,14 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
+APP_ID = 6604044
 
 def get_user_login():
-    pass
+    return input('Enter your login: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass('Enter your password: ')
 
 
 def get_online_friends(login, password):
@@ -17,16 +16,24 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api = vk.API(session, v='5.8')
+    id_of_online_friends = api.friends.getOnline()
+    return api.users.get(user_ids=id_of_online_friends)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print(friend['first_name'], ' ', friend['last_name'])
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    if login and password:
+        friends_online = get_online_friends(login, password)
+        print('\nList of online users:\n')
+        output_friends_to_console(friends_online)
+    else:
+        print('You forget to enter login or password')
